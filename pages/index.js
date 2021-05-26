d32b0528a4d7fbc0670d14337567838b8f48a421
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-05-26 12:17:52 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <Head>
+        <title>Test-Dernier-Cri</title>
+      </Head>
+      <main class="container">
+        <ul>
+          {posts.map((post) => (
+            <li>
+              <Link href={`/articles/${post.id}`}>
+                <a>
+                  <div class="content">
+                    <h3>{post.title}</h3>
+                  </div>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const posts = await fetch(
+    "http://jsonplaceholder.typicode.com/posts?_limit=10"
+  ).then((r) => r.json());
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 5,
+  };
+}
